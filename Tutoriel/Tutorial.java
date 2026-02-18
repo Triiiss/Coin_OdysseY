551f8d479bff05d62d9ea4b33737655506bfcc1f
Tris <166401431+Triiiss@users.noreply.github.com>
2026-02-18 11:07:44 +0100

    Update tutorial

---
+++ Tutoriel/Tutorial.java
@@ -0,0 +1,12 @@
+public class Tutorial{
+    /**
+     * Empty constructor
+     */
+    private Tutorial(){
+        // Empty constructor
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
