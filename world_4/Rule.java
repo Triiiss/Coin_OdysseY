2a00919e4291d5c4720b0bd0843bdde7b37028cf
Tris <166401431+Triiiss@users.noreply.github.com>
2026-03-18 09:55:03 +0100

    Add color to terminal and enemy behavior
    Add comments too

--- world_4/Rule.java
+++ world_4/Rule.java
@@ -6,58 +6,94 @@
 package world_4;
 
 import java.util.Scanner;
+import java.util.Random;
+import java.util.List;
+import java.util.Iterator;
+import java.util.HashSet;
 
+/**
+ * The rules of the game, comportemental things
+ */
 public class Rule{
-    public Rule(Level level){
+    /**
+     * Empty constructor
+     */
+    public Rule(){
     }
 
     /**
      * Each cell has a type, and shows something based on what's on it
      * The player is priority, then coins then the actual type
      * So a coin can be on a trap and just show the coin (it's a hidden trap hehe)
-     * @param playerX The X coordinate of the player
-     * @param playerY The coordinates of the player to know if the player is in the cell or not
+     * @param cell The cell we want to give the char of
+     * @param enemies The list of enemies
+     * @param playerCoord the player's coordinate
      * @return the character of the cell ( ,.,#,*,1,etc)
      */
-    public static char cellChar(Cell cell,Enemy[] enemies, Position playerCoord){
-        if (playerCoord.equals(cell.getX(),cell.getY())){
-            return '1';
+    public static String cellChar(Cell cell,HashSet<Cell> enemyCells, Position playerCoord){
+        String RESET = "\u001B[0m";
+        String YELLOW = "\u001B[33m";  // Coin
+        String RED = "\u001B[31m";      // Enemy
+        String CYAN = "\u001B[36m";     // Player
+        String MAGENTA = "\u001B[35m";  // Traps
+
+        if (playerCoord.equals(cell.getCoord())){
+            return CYAN + "1" + RESET;
         }
-        for (int i=0;i<enemies.length;i++){
-            if (enemies[i].coord.equals(cell.getX(),cell.getY())){
-                return 'R';
+        for (Cell e : enemyCells){
+            if (e.equals(cell)){
+                return RED + "R" + RESET;
             }
         }
 
         if (cell.getCoin()){
-            return '.';
+            return YELLOW + "." + RESET;
         }
         switch(cell.getType()){
             case CellType.WALL:
-                return '#';
+                return "#" + RESET;
             case CellType.EMPTY:
-                return ' ';
+                return " " + RESET;
             case CellType.TRAP:
-                return '*';
+                return MAGENTA + "*" + RESET;
             case CellType.DOOR:
-                return 'D';
+                return "D" + RESET;
         }
-        return ' ';
+        return " ";
     }
 
+    /**
+     * Checks if the game is over (player no health)
+     * @param level the level with the player in it
+     * @return if the game is over or not
+     */
     public static boolean gameOver(Level level){
         return level.getPlayer().getHealthPoint() <= 0;
     }
 
+    /**
+     * Checks if a level is done (no coins left)
+     * @param level the level
+     * @return if the level is completed or not
+     */
     public static boolean levelComplete(Level level){
         return level.getNbCoins() <= 0;
     }
 
+    /**
+     * Asks an input to the user
+     * @return the input as a letter
+     */
     public static String getInput(){
         Scanner sc = new Scanner(System.in);
         return sc.next();
     }
 
+    /**
+     * Link the letters of the input to the direction
+     * @param input the input of the user
+     * @return the direction
+     */
     public static Direction keyBinding(String input){
         char leftKey = 'q';
         char upKey = 'z';
@@ -89,10 +125,19 @@ public class Rule{
         return Direction.UNKNOWN;
     }
 
+    /**
+     * Ask input and return the direction
+     * @return the direction given by the user
+     */
     public static Direction getDirection(){
         return Rule.keyBinding(getInput());
     }
 
+    /**
+     * The tore mecanic (loop)
+     * @param level the level it is in
+     * @param coord the coords that needs changing if it is out of bound
+     */
     public static void tore(Level level, Position coord){
         while (coord.getX() < 0){         // The tore system => adds a cycle to the coordinates
             coord.addX(level.getWidth());
@@ -108,17 +153,83 @@ public class Rule{
         }
     }
 
-    public static void enemyHit(Level level, Enemy enemy, Position newPlayer){
-        
+    /**
+     * When an enemy and a player a colliding
+     * @param enemy The enemy
+     * @param player The player
+     * @return if the action worked or not
+     */
+    public static boolean enemyHit(Enemy enemy, Player player){
+        switch(enemy.getType()){
+            case EnemyType.RANDOM:
+                player.removeHealth(1);
+                break;
+            default:
+                return false;
+        }
+        System.out.println("\u001B[31mYou've been hit\u001B[0m");
+        return true;
     }
 
+    /**
+     * Moves an enemy based on its type
+     * @param level the level where the enemy moves
+     * @param enemy the moving enemy
+     */
+    public static void moveEnemy(Level level, Enemy enemy){
+        switch(enemy.getType()){
+            case EnemyType.RANDOM:
+                Rule.enemyRandomMove(level,enemy);
+                break;
+        }
+    }
+
+    /**
+     * Moves the enemy with the type RANDOM
+     * @param level the level where the enemy moves
+     * @param enemy the moving enemy
+     */
+    private static void enemyRandomMove(Level level, Enemy enemy){
+        Position newEnemy = new Position(enemy.getCoord().getX(),enemy.getCoord().getY());
+        Random rand = new Random();
+        switch(rand.nextInt(4)){
+            case 0:
+                newEnemy.addX(1);
+                break;
+            case 1:
+                newEnemy.addY(1);
+                break;
+            case 2:
+                newEnemy.addX(-1);
+                break;
+            case 3:
+                newEnemy.addY(-1);
+                break;
+        }
+
+        if (level.isAvailable(newEnemy,enemy)){
+            enemy.move(newEnemy.getX(),newEnemy.getY());
+        }
+    }
+
+    /**
+     * Activate a trap a hurt the player
+     * @param level The level we activate the trap from
+     * @param newPlayer the player's future position
+     */
     public static void activateTrap(Level level,Position newPlayer){
         level.getPlayer().removeHealth(2);
+        level.resetEnemies();
         level.getLevel()[newPlayer.getY()][newPlayer.getX()].setType(CellType.EMPTY);       // Delete the trap
         newPlayer.setX(level.getStartPlayer().getX());
         newPlayer.setY(level.getStartPlayer().getY());
     }
 
+    /**
+     * Collect the coin from the cell
+     * @param level The level we take the coin from
+     * @param newPlayer the player's future position
+     */
     public static void collectCoin(Level level, Position newPlayer){
         level.getPlayer().addScore(10);
         level.getLevel()[newPlayer.getY()][newPlayer.getX()].removeCoin();
