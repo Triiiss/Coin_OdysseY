58fbdca1104d72cf78986085c8eab3593bea5afb
Tris <166401431+Triiiss@users.noreply.github.com>
2026-04-04 23:18:26 +0200

    Add items and competence

---
+++ world_5/inventory/Competence.java
@@ -0,0 +1,43 @@
+/**
+ * @author Thémis Tran Tu Thien :D
+ * @version 1.0
+ */
+
+package world_5.inventory;
+
+import world_5.types.CompetenceType;
+import world_5.environnement.Level;
+import world_5.environnement.Cell;
+import world_5.characters.Player;
+
+/**
+ * Competence class
+ */
+public class Competence extends Element{
+    private CompetenceType type;
+
+    public Competence(String name, CompetenceType type){
+        super(name);
+        this.type = type;
+    }
+
+    public CompetenceType getType(){
+        return this.type;
+    }
+    
+    public boolean pickUp(Level level){
+        if (level.getPlayer().getInventorySpace() < level.getPlayer().getMaxInventory()){
+            switch(this.type){
+                case CompetenceType.LOCKPICK:
+                    break;
+                case CompetenceType.TELEPORTATION:
+                    break;
+            }
+        }
+        return false;
+    }
+
+    public void use(){
+        
+    }
+}
---
+++ world_5/inventory/Item.java
@@ -0,0 +1,58 @@
+/**
+ * @author Thémis Tran Tu Thien :D
+ * @version 1.0
+ */
+
+package world_5.inventory;
+
+import world_5.types.*;
+import world_5.environnement.Cell;
+import world_5.environnement.Level;
+import world_5.characters.Player;
+
+/**
+ * Item class
+ */
+public class Item extends Element{
+    private boolean consumable;
+    private ItemType type;
+
+    public Item(String name, ItemType type){
+        super(name);
+        this.consumable = type.defaultConsumable();
+        this.type = type;
+    }
+
+    public boolean getConsumable(){
+        return this.consumable;
+    }
+
+    public ItemType getType(){
+        return this.type;
+    }
+    
+    public boolean pickUp(Level level){
+        if (this.consumable){
+            switch (this.type){
+                case ItemType.COIN:
+                    level.getPlayer().addScore(10);
+                    level.removeOneNbCoin();
+                    return true;
+            }
+        }
+        else if (level.getPlayer().getInventorySpace() < level.getPlayer().getMaxInventory()){      // If there is space
+            level.getPlayer().addInventory(this);
+            return true;
+        }
+        return false;
+    }
+
+    public void use(){
+        switch(this.type){
+            case ItemType.WEAPON:
+                break;
+            case ItemType.HOURGLASS:
+                break;
+        }
+    }
+}
