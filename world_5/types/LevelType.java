2b8901d737f6ffa436492a5a9844c1b777718376
Tris <166401431+Triiiss@users.noreply.github.com>
2026-04-07 13:05:37 +0200

    Add different types of level

---
+++ world_5/types/LevelType.java
@@ -0,0 +1,25 @@
+/**
+ * @author Thémis Tran Tu Thien :D
+ * @version 1.0
+ */
+
+package world_5.types;
+
+/**
+ * All the types of level
+ */
+public enum LevelType{
+    /**To finish the level, you have to collect all the coins */
+    COINS,
+    /**To finish the level, you have to defeat all enemies */
+    ENEMIES;
+
+    /**
+     * Gets an int and checks if it corresponds to a type (used during creation, and file reading)
+     * @param type The type of the level
+     * @return If the type is valid or not
+     */
+    public static boolean isValidType(int type){
+        return type >= 0 && type < CellType.values().length;
+    }
+}
